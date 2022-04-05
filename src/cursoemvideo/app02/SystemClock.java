98e3dbc789302f0aaeae96ec6cf5d044bc003a39
Pedro de Castilho <pedrodecastilho@protonmail.com>
2022-04-05 16:40:33 -0300

    add SystemClock app

---
+++ src/cursoemvideo/app02/SystemClock.java
@@ -0,0 +1,10 @@
+package cursoemvideo.app02;
+
+import java.util.Date;
+
+public class SystemClock {
+    public static void main(String[] args) {
+        Date clock = new Date();
+        System.out.println("The current time is: " + clock.toString());
+    }
+}
